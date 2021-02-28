d619148507e53fa7b4600d74b8d95104bad9341f
Nix Genco-Kamin <nix@pop-os.localdomain>
2021-02-28 12:01:02 -0800

    Adds dynamic location, automatic with geocoder or manual

--- home.py
+++ home.py
@@ -2,16 +2,16 @@
 import requests
 from startup import startup
 startup()
-import config
-import json
+# import config
 
-get_weather = requests.get(
-    f'http://api.weatherapi.com/v1/current.json?key={config.api_key}&q=Los%20Angeles'
-)
 
-weather = json.loads(get_weather.text)
-print(weather["location"]["localtime"])
-print(
-    f'Current Temp: {weather["current"]["temp_f"]}°f | Feels like: {weather["current"]["feelslike_f"]}°f'
-)
-print(f'Condition: {weather["current"]["condition"]["text"]}')
+# get_weather = requests.get(
+#     f'http://api.weatherapi.com/v1/current.json?key={config.api_key}&q=Los%20Angeles'
+# )
+
+# weather = json.loads(get_weather.text)
+# print(weather["location"]["localtime"])
+# print(
+#     f'Current Temp: {weather["current"]["temp_f"]}°f | Feels like: {weather["current"]["feelslike_f"]}°f'
+# )
+# print(f'Condition: {weather["current"]["condition"]["text"]}')
--- startup.py
+++ startup.py
@@ -1,18 +1,59 @@
 import os
 from os import path
 import pyinputplus as pyip
+import geocoder
+import json
+
+
+def get_location(set_type):
+    if set_type=="Automatic":
+        city = geocoder.ip("me").city
+        confirm_city = pyip.inputYesNo(f"Does '{city}' look right? [Y/n] ")
+        if confirm_city == "no":
+            print("Sorry about that, let's try entering your city manually.")
+            set_type = "Manual"
+            get_location(set_type)
+    else:
+        city = input("Please enter your city name: ").capitalize()
+        confirm_city = pyip.inputYesNo(f"Does '{city}' look right? [Y/n] ")
+        if confirm_city == "no":
+            print("Let's try that again.")
+            get_location(set_type)
+    return city
+
 
 
 def startup():
-    if not path.isfile("config.py"):
-        config = open("config.py", "w")
+    if not path.isfile("config.json"):
+        settings = {
+            "api_key": "",
+            "default_forecast": 7,
+            "view": "",
+            "city": ""
+        }
+
         api_key = input("Please enter your api key here: ")
-        config.write(f"api_key = '{api_key}' \n")
         default_forecast = pyip.inputNum(
             min=1, max=7, prompt="How many days forecast by default? (1-7): ")
-        config.write(f"default_forecast = {default_forecast} \n")
-        result = pyip.inputMenu(
+        # config.write(f"default_forecast = {default_forecast} \n")
+        view = pyip.inputMenu(
             ['Minimal', 'Aesthetic'],
             numbered=True,
             prompt="Do you prefer minimalism or ASCII art?: \n")
-        config.write(f"view = '{result}' \n")
+        # config.write(f"view = '{result}' \n")
+        locset = pyip.inputMenu(
+            ['Automatic', 'Manual'],
+            numbered=True,
+            prompt="Would you prefer to have your city set automatically or manually?: \n")
+        city = get_location(locset)
+        settings["api_key"] = api_key
+        settings["city"] = city
+        settings["view"] = view
+        settings["default_forecast"] = default_forecast
+        settings_json = json.dumps(settings)
+        config = open("config.json", "w")
+        config.write(settings_json)        
+
+
+
+
